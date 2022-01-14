c8d92f238892a8547a05f6d757123f31abec51b4
Sebastian <sebastian.lievanoarzayus@mail.utoronto.ca>
2022-01-14 00:58:53 -0500

    Added input stream management (cin.clear() and cin.ignore() ) to avoid accidental menu skipping

--- gradebook.cpp
+++ gradebook.cpp
@@ -1,20 +1,16 @@
-#include "globals.h"
 #include "course.h"
-#include "task.h"
 
 vector<course> classes;
 
 //Helper Functions
 int openingMenu();
-void load(vector<course>& s);
-void save(const vector<course> s);
 void classManagement();
 
+void clearPage();
 
 int main(){
     int userInput;
     bool keepGoing = true;
-    load(classes);
     while(keepGoing){
         clearPage();
         userInput = openingMenu();
@@ -23,7 +19,6 @@ int main(){
         else if(userInput == 0) classManagement();
         else classes[userInput - 1].menu();
     }
-    save(classes);
     return 0;
 }
 
@@ -39,16 +34,26 @@ int openingMenu(){
     cout << "Menu Options: " << endl << "Enter corresponding number to access Class Menus"
     << endl << "0) Manage Classes" << endl << "Anything else to exit" << endl << ">";
     cin >> userInput;
+    cin.ignore(mil, '\n');
     if(!cin.fail() && userInput > 0 && userInput < numClasses){
+        cin.clear();
         return userInput;
+    }
+    else if(userInput == 0){
+        classManagement();
+        goto restart;
     }
     else {
         userInput = -1;
         cout << "Invalid Input. Are you sure you want to exit the program?" << endl
         << "Input 1 to exit. Input any other key to continue" << endl << ">";
         cin >>  userInput;
+        cin.ignore(mil, '\n');
         if(userInput == 1) return -1;
-        if(cin.fail() || userInput != 1) goto restart;
+        if(cin.fail() || userInput != 1){
+            cin.clear();
+            goto restart;
+        }
     }
     return -1;
 }
@@ -67,11 +72,13 @@ void classManagement(){
     cout << "0) Return to Main menu" << endl << "1) Add Class" << 
     endl << "2) Delete Class" << endl;
     cin >> input;
+    cin.ignore(mil, '\n');
     switch(input){
         case 0: return; break;
         case 1: { 
             cout << "Please enter the name of your new class" << endl;
             cin >> inputString;
+            cin.ignore(mil, '\n');
             course c(inputString);
             classes.push_back(c);
             c.menu();
@@ -80,7 +87,9 @@ void classManagement(){
         case 2: {
             cout << "Please enter the number of the class you'd like to delete";
             cin >> input;
+            cin.ignore(mil, '\n');
             if(cin.fail() || input < 1 || input > numClasses){
+                cin.clear();
                 cout << "Invalid Input" << endl;
 
             }
@@ -88,7 +97,11 @@ void classManagement(){
                 cout << "Are you sure you want to erase " << classes[input-1].getName() << "?" << endl;
                 cout << "Press 1 to confirm, anything else to cancel" << endl;
                 cin >> input;
-                if(cin.fail() || input != 1) cout << "Cancelled" << endl;
+                cin.ignore(mil, '\n');
+                if(cin.fail() || input != 1){
+                    cin.clear();
+                    cout << "Cancelled" << endl;
+                }
                 else classes.erase(classes.begin()+input-1);
             }
         }
