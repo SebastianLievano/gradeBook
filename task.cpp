3fba64f57122340091afc2d9b1d45fafffce393c
Sebastian <sebastian.lievanoarzayus@mail.utoronto.ca>
2022-01-14 21:18:07 -0500

    Finalized task.cpp

--- task.cpp
+++ task.cpp
@@ -1,19 +1,28 @@
+/*
+task.cpp by Sebastian Lievano Arzayus
+Part of GradeBook C++ program
+Class method declarations for task class
+
+*/
+
 #include "task.h"
 
-
+//Constructors
+//Constructor with mark input sets all values upon construction
 task::task(string n, double w, double m){
     name = n;
     weight = w;
     mark = m;
 }
-        
+
+//Constructor without mark input sets mark as empty
 task::task(string n, double w){
     name = n;
     weight = w;
     mark = empty;
 }
 
-//Accessors
+//Accessors for weight, mark, and name
 double task::getWeight(){
     return weight;
 }
@@ -26,7 +35,7 @@ string task::getName(){
     return name;
 }
 
-//Mutators
+//Mutators for weight, mark, and name
 void task::setWeight(double w){
     weight = w;
 }
@@ -38,29 +47,33 @@ void task::setMark(double m){
 void task::setName(string n){
     name = n;
 }
-/*Keeping Dots in Line:
 
-MARK
-100.00%
- 50.00%
-  5.00%
-TASK NAME      WEIGHT     MARK
-*/
+
+
 //Utility
+//METHOD task::print : prints the task name, weight, and mark
 void task::print(){
+    //Setting output to two decimal points
     cout << fixed;
     cout << setprecision(2);
     cout << name;
+    //Printing 15 characters combined of name and spaces. 
+    //Max name length is 12 characters, but name box must be same length for all tasks
     for(int i = name.length(); i < 15; i++) cout << " ";
+    //If the weight value is single digit, an extra space is added to keep things aligned
     if(weight < 10) cout << " ";
     cout << weight << "%";
     cout << "     ";
+    //If mark is single digit, two spaces are added
     if(mark < 10) cout << "  ";
+    //If mark is double digit, single space added. if 100%, no space added
     else if(mark != 100) cout << " ";
+    //Only printing if mark is set
     if(mark != empty) cout << mark << "%";
     cout << endl;
 }
 
+//METHOD task::percentsGotten : returns the percentage points the student received from that task
 double task::percentsGotten(){
     return ((mark*weight)/100);
 }
